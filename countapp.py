1fda51a1ee43b18142825bf4eec47465c5f73307
中山城次 <nijineko-2425@outlook.jp>
2026-07-25 17:49:19 +0900

    1,ボタンを操作した際、メッセージが表示されるようになりました。 2,そのほか軽微な変更

--- countapp.py
+++ countapp.py
@@ -4,8 +4,11 @@ if "count" not in st.session_state:
     st.session_state.count=0
 if st.button("1追加"):
     st.session_state.count+=1
+    st.write("カウントを1増やしました")
 if st.button("1減少"):
     st.session_state.count-=1
+    st.write("カウントを1減らしました")
 if st.button("リセット"):
     st.session_state.count=0
-st.write(f"現在のカウント:{st.session_state.count}")
+    st.write("カウントを0にリセットしました")
+st.write(f"現在の数値:{st.session_state.count}")
