2ee00e737f18e8e53894851c1167da3f6e4560f7
中山城次 <nijineko-2425@outlook.jp>
2026-07-25 17:18:50 +0900

    カウントを減らすためのボタンを追加

--- countapp.py
+++ countapp.py
@@ -3,4 +3,6 @@ if "count" not in st.session_state:
     st.session_state.count=0
 if st.button("1追加"):
     st.session_state.count+=1
+if st.button("1減少"):
+    st.session_state.count-=1
 st.write(f"現在のカウント:{st.session_state.count}")
