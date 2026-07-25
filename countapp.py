c1f59909e2bc84e98c1bb15889057769d536f840
中山城次 <nijineko-2425@outlook.jp>
2026-07-25 17:43:32 +0900

    本ツールのタイトル及びリセットボタンを追加いたしました

--- countapp.py
+++ countapp.py
@@ -1,8 +1,11 @@
 import streamlit as st
+st.title("シンプルカウントツール")
 if "count" not in st.session_state:
     st.session_state.count=0
 if st.button("1追加"):
     st.session_state.count+=1
 if st.button("1減少"):
     st.session_state.count-=1
+if st.button("リセット"):
+    st.session_state.count=0
 st.write(f"現在のカウント:{st.session_state.count}")
